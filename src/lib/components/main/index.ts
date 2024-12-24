40d1ef247efc87f74e3a392b3e9065996b87a5f9
Alexey Maltsev <axel.user.perm@gmail.com>
2024-12-24 14:52:51 +0100

    Removed redundant code

--- src/lib/components/main/index.ts
+++ src/lib/components/main/index.ts
@@ -1,2 +1,1 @@
-export { default as DimensionsInput } from "./dimensions-input.svelte";
-export { default as RegionFilter } from "./region-filter.svelte";
+export { default as RegionFilter } from './region-filter.svelte';
