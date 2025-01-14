1ac02326741fad025fcc65494d8de6517d5870ed
Alexey Maltsev <axel.user.perm@gmail.com>
2025-01-14 18:25:11 +0100

    Added validation to disable analytics if variables are missing or invalid.

--- src/lib/analytics/posthog-analytics.ts
+++ src/lib/analytics/posthog-analytics.ts
@@ -1,7 +1,7 @@
 import { AnalyticsService } from './analytics-service';
 import type { EventProperties } from './analytics-service';
 import { posthog } from 'posthog-js';
-import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
+import * as env from '$env/static/public';
 import { isLocalhost } from '$lib/utils';
 import { browser } from '$app/environment';
 
@@ -9,14 +9,27 @@ export class PosthogAnalytics extends AnalyticsService {
 	constructor() {
 		super();
 
-		if (browser && !isLocalhost()) {
-			posthog.init(PUBLIC_POSTHOG_API_KEY, {
-				api_host: 'https://us.i.posthog.com',
-				person_profiles: 'always',
-				persistence: 'memory',
-				autocapture: false
-			});
+		if (
+			!('PUBLIC_POSTHOG_API_KEY' in env) ||
+			!('PUBLIC_POSTHOG_HOST' in env) ||
+			typeof env.PUBLIC_POSTHOG_API_KEY !== 'string' ||
+			typeof env.PUBLIC_POSTHOG_HOST !== 'string'
+		) {
+			console.warn('PostHog API key or host not found or invalid. Analytics will be disabled.');
+			return;
 		}
+
+		if (!browser || isLocalhost()) {
+			console.warn('Analytics will be disabled in non-browser or localhost environments.');
+			return;
+		}
+
+		posthog.init(env.PUBLIC_POSTHOG_API_KEY, {
+			api_host: env.PUBLIC_POSTHOG_HOST,
+			person_profiles: 'always',
+			persistence: 'memory',
+			autocapture: false
+		});
 	}
 
 	protected trackEventInternal(eventName: string, properties?: EventProperties): void {
