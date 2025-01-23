5a98f4e9903c8c84e4fae02086f50addd2e57e15
Alexey Maltsev <axel.user.perm@gmail.com>
2025-01-23 20:20:07 +0100

    Added info component to storybook

---
+++ src/stories/Info.stories.svelte
@@ -0,0 +1,27 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Info from '$lib/components/main/info.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Main/Info',
+		component: Info,
+		tags: ['autodocs'],
+		argTypes: {
+			coveredByTest: {
+				description: 'The number of airlines covered by the automated test',
+				control: 'number'
+			},
+			lastTestRun: {
+				description: 'The date of the last test run',
+				control: 'date'
+			}
+		}
+	});
+
+	const mockData = {
+		coveredByTest: 25,
+		lastTestRun: new Date('2024-03-20')
+	};
+</script>
+
+<Story name="Default" args={mockData} />
