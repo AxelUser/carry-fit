810582b79649035f8753184d05fc3235bd08b477
Alexey Maltsev <axel.user.perm@gmail.com>
2025-01-25 01:22:12 +0100

    Added story for compliance score

---
+++ src/stories/main/compliance-score.stories.svelte
@@ -0,0 +1,63 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import ComplianceScore from '$lib/components/main/compliance-score.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Main/ComplianceScore',
+		component: ComplianceScore,
+		tags: ['autodocs'],
+		parameters: {
+			layout: 'centered'
+		},
+		argTypes: {
+			allAirlinesCount: {
+				description: 'Total number of airlines',
+				control: 'number'
+			},
+			compliantAirlinesCount: {
+				description: 'Number of airlines that comply',
+				control: 'number'
+			}
+		}
+	});
+</script>
+
+<Story
+	name="High Score"
+	args={{
+		allAirlinesCount: 10,
+		compliantAirlinesCount: 9
+	}}
+/>
+
+<Story
+	name="Medium Score"
+	args={{
+		allAirlinesCount: 10,
+		compliantAirlinesCount: 7
+	}}
+/>
+
+<Story
+	name="Low Score"
+	args={{
+		allAirlinesCount: 10,
+		compliantAirlinesCount: 5
+	}}
+/>
+
+<Story
+	name="Zero Score"
+	args={{
+		allAirlinesCount: 10,
+		compliantAirlinesCount: 0
+	}}
+/>
+
+<Story
+	name="No Airlines"
+	args={{
+		allAirlinesCount: 0,
+		compliantAirlinesCount: 0
+	}}
+/>
