a41fda2670951819bba516d8b32ea527f9bd3c22
AxelUser <AxelUser@users.noreply.github.com>
2025-02-04 00:21:22 +0000

    deploy: 0328970a08b44b25eff4979d24d8ea5f0c5f779e

--- _app/immutable/chunks/navigation.C9z9Rdzv.js
+++
@@ -1,1 +0,0 @@
-import{b as l}from"./paths.ByBu2SDf.js";const e={legal:{privacy:`${l}/legal/privacy`,terms:`${l}/legal/terms`,optOut:`${l}/legal/opt-out`},home:`${l}/`};export{e as l};
---
+++ _app/immutable/chunks/utils.CCBnQ-pT.js
@@ -0,0 +1,1 @@
+import{n as b,q as e}from"./runtime.BxvHVIq_.js";function c(n,r,s){if(n==null)return r(void 0),s&&s(void 0),b;const u=e(()=>n.subscribe(r,s));return u.unsubscribe?()=>u.unsubscribe():u}export{c as s};
