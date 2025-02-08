a696d5afa450621e9551decb9d5b54ae73aa3beb
AxelUser <AxelUser@users.noreply.github.com>
2025-02-08 15:23:16 +0000

    deploy: 167f79ccf472626c867bd5523f22bd9442aa3596

--- _app/immutable/chunks/navigation.ARRgM2NZ.js
+++
@@ -1,1 +0,0 @@
-import{b as l}from"./paths.Ov5KeYdS.js";const e={legal:{privacy:`${l}/legal/privacy`,terms:`${l}/legal/terms`,optOut:`${l}/legal/opt-out`},home:`${l}/`};export{e as l};
---
+++ _app/immutable/entry/start.BeSJMg-k.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DUZg-hZY.js";export{t as start};
