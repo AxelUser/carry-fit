3566f2ba952257a38ec725b02166c3df7eed7578
Alexey Maltsev <axel.user.perm@gmail.com>
2025-12-20 11:50:46 +0100

    styles: changed text color for click-splash effect

--- src/lib/components/ui/click-splash/click-splash.svelte
+++ src/lib/components/ui/click-splash/click-splash.svelte
@@ -49,7 +49,7 @@
 				class="pointer-events-none absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2"
 				transition:fly={{ duration, easing: cubicOut }}
 			>
-				<span class={cn('animate-float-away block font-medium text-gray-700', sizeClasses[size])}>
+				<span class={cn('animate-float-away text-foreground block font-medium', sizeClasses[size])}>
 					{content}
 				</span>
 			</div>
