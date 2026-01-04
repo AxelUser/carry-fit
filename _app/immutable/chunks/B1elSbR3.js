f295fed2c202ab013b29ebf40364cc8f58798db3
AxelUser <AxelUser@users.noreply.github.com>
2026-01-04 12:29:49 +0000

    deploy: 1a88cc47fa1315925ede55e0b696390df9c432bb

---
+++ _app/immutable/chunks/B1elSbR3.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_x34onm?.base??"",a=globalThis.__sveltekit_x34onm?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/CaVX-gCr.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_a4fkid?.base??"",a=globalThis.__sveltekit_a4fkid?.assets??s??"";export{a,s as b};
